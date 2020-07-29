49864dab7216753e467145d560a047d543bfe30d
millni <miero@daum.net>
2020-07-30 06:45:50 +0900

    임시

--- src/App.js
+++ src/App.js
@@ -1,11 +1,37 @@
 import React from 'react';
 
-function App() {
-  return (
-    <div>
-      <h1>Hello</h1>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    console.log('hello');
+  }
+  state={
+    count: 0,
+  };
+
+  add = ()=>{
+    this.setState(current => ({count: current.count + 1}));
+  }
+
+  minus = ()=>{
+    this.setState(current => ({count: current.count - 1}));
+  }
+
+  componentDidMount(){
+    console.log('component redered');
+  }
+
+  render() {
+    console.log("I'm rendering");
+    return (
+      <div>
+        <h1>I'm a class Component</h1>
+        <h2>The number is: {this.state.count}</h2>
+        <button onClick={this.add}>Add</button>
+        <button onClick={this.minus}>Minus</button>
+      </div>
+    );
+  }
 }
 
 export default App;
