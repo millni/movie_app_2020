7a890e566bf86a8db74ede4b6d702090224d35cc
millni <miero@daum.net>
2020-07-30 07:20:46 +0900

    준비

--- src/App.js
+++ src/App.js
@@ -1,34 +1,24 @@
 import React from 'react';
 
 class App extends React.Component {
-  constructor(props){
-    super(props);
-    console.log('hello');
-  }
-  state={
-    count: 0,
+  state = {
+    isLoading: true,
+    movies: [],     /* 로딩된 영화 데이터...*/
   };
 
-  add = ()=>{
-    this.setState(current => ({count: current.count + 1}));
-  }
-
-  minus = ()=>{
-    this.setState(current => ({count: current.count - 1}));
-  }
-
-  componentDidMount(){
-    console.log('component redered');
-  }
+componentDidMount(){
+  // 영화 데이터 로딩
+  setTimeout(()=>{
+    this.setState({isLoading: false});
+  }, 6000);
+}
 
   render() {
-    console.log("I'm rendering");
-    return (
+    const { isLoading } = this.state;
+     return (
       <div>
-        <h1>I'm a class Component</h1>
-        <h2>The number is: {this.state.count}</h2>
-        <button onClick={this.add}>Add</button>
-        <button onClick={this.minus}>Minus</button>
+        {isLoading ? 'Loading...' : 'We are ready'}
+        
       </div>
     );
   }
