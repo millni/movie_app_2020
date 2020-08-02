e17971d63b0c1abcb9079ec721cb06a15a9d74e9
millni <miero@daum.net>
2020-08-03 07:17:08 +0900

    p176

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import axios from 'axios';
+import Movie from './Movie';
 
 class App extends React.Component {
   state = {
@@ -6,19 +8,36 @@ class App extends React.Component {
     movies: [],     /* 로딩된 영화 데이터...*/
   };
 
-componentDidMount(){
-  // 영화 데이터 로딩
-  setTimeout(()=>{
-    this.setState({isLoading: false});
-  }, 6000);
-}
+  getMovies = async () => {
+    const {data:{data:{movies}}} = await axios.get("https://yts.mx/api/v2/list_movies.json?sort_by=rating");
+    this.setState({ movies, isLoading: false });
+  };
+
+  componentDidMount(){
+    // 영화 데이터 로딩
+    this.getMovies();
+    // axios.get('https://yts.mx/api/v2/list_movies.json');
+  }
 
   render() {
-    const { isLoading } = this.state;
-     return (
+    const { isLoading, movies } = this.state;
+    return (
       <div>
-        {isLoading ? 'Loading...' : 'We are ready'}
-        
+        {isLoading 
+          ? 'Loading...' 
+          : movies.map((movie) => {
+              console.log(movie);
+              return (
+                <Movie
+                  key={movie.id}
+                  id={movie.id}
+                  year={movie.year}
+                  title={movie.title}
+                  summary={movie.summary}
+                  poster={movie.medium_cover_image}
+                />
+              );
+            })}
       </div>
     );
   }
