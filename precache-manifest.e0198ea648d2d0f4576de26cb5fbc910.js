dd39e704a69c4d5fa9b9915229c7009b5c329174
millni <miero@daum.net>
2020-08-05 07:23:15 +0900

    Updates

---
+++ precache-manifest.e0198ea648d2d0f4576de26cb5fbc910.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dee03acf895f39e86a07e5daabffcdb8",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "48b9a704239954ea144b",
+    "url": "/movie_app_2020/static/css/main.f2cb9dfe.chunk.css"
+  },
+  {
+    "revision": "da91fca76777d8e65fc5",
+    "url": "/movie_app_2020/static/js/2.e2494b61.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movie_app_2020/static/js/2.e2494b61.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "48b9a704239954ea144b",
+    "url": "/movie_app_2020/static/js/main.9f71f055.chunk.js"
+  },
+  {
+    "revision": "6636fcdddd453d60abc3",
+    "url": "/movie_app_2020/static/js/runtime-main.9a083715.js"
+  }
+]);
